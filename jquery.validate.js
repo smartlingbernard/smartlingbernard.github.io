b1c157f0d3058221b9fd601807d343d909c4fa64
smartlingbernard <bnowbahar@smartling.com>
2017-01-03 10:59:20 -0500

    Update jquery.validate.js

--- jquery.validate.js
+++ jquery.validate.js
@@ -348,9 +348,8 @@ $.extend( $.validator, {
 	setDefaults: function( settings ) {
 		$.extend( $.validator.defaults, settings );
 	},
-/* <sl:translate_json> */ 
+	/*<sl:translate>*/
 	messages: {
-		"sl_translate": "sl_all",
 		required: "This field is required.",
 		remote: "Please fix this field.",
 		email: "Please enter valid email address",
@@ -368,7 +367,8 @@ $.extend( $.validator, {
 		min: $.validator.format( "Please enter a value greater than or equal to {0}." ),
 		step: $.validator.format( "Please enter a multiple of {0}." )
 	},
-/* </sl:translate_json> */
+	/*</sl:translate>*/
+
 	autoCreateRanges: false,
 
 	prototype: {
